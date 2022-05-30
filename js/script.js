a9f400a57b482ed371cae3ac9d150c4eeaff29d1
sunnyyuanz <you@example.com>
2022-05-29 22:03:36 -0400

    added pokemonlist and foodlist arrays to the js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-alert('Hello world');
 
-let favoriteFood = 'sushi';
+// Followed Task 1.2 instruction of building a pokemonlist but now directly related to this project. 
+let pokemonList = [
+    {name:"Charmeleon", height:1.1, types:["steel", "fire", "ice", "fairy"]},
+    {name:"Beedrill", height:1, types:["poison", "bug", "fairy", "fighting", "grass"]},
+    {name:"Pikachu", height:0.4, types:["flying", "steel", "electric"]}
+];
 
-document.write(favoriteFood);
+//Something might be really related to this project which is a foodlist. 
+let foodList = [
+    {foodname:"Sushi", healthiness_score:5, ingredients:["salmon", "avocado", "rice", "cucumber", "seeweed"]},
+    {foodname:"Pasta", healthiness_score:4, ingredients:["chicken", "broccoli", "mushrooms", "tomatoes", "Alfred Sauce", "Fettuccine"]},
+    {foodname:"Burger", healthiness_score:3.8, ingredients:["beef", "onion", "hamburger buns", "mayonnaise", "ketchup", "lettuce leaves", "tomato"]},
+];
+
