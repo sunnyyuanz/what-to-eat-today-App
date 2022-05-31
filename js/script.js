e8a5bfc1a4be3f63dde5536218fa73cfd22bdcf6
sunnyyuanz <you@example.com>
2022-05-31 00:28:37 -0400

    updated all for loop to forEach loop

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-// Followed Task 1.2 instruction of building a pokemonlist but now directly related to this project. 
-let pokemonList = [
-    { name: "Charmeleon", height: 1.1, types: ["steel", "fire", "ice", "fairy"] },
-    { name: "Beedrill", height: 1, types: ["poison", "bug", "fairy", "fighting", "grass"] },
-    { name: "Pikachu", height: 0.4, types: ["flying", "steel", "electric"] }
-];
-
 //Something might be really related to this project which is a foodlist. 
 let foodList = [
     { foodname: "Sushi", healthiness_score: 5, ingredients: ["salmon", "avocado", "rice", "cucumber", "seeweed"] },
@@ -15,32 +7,30 @@ let foodList = [
 
 //Starting pull data from array to display in Html
 const food = document.querySelector(".food ul");
+let goodFood = {healthiness_score:0}
 
-for (i = 0; i < foodList.length; i++) {
+//determine which food has the highest healthiness score
+foodList.forEach(food => {
+    if(food.healthiness_score>goodFood.healthiness_score){
+        goodFood = food
+    }
+})
+
+foodList.forEach(foodinfo => {
     const node = document.createElement("li");
-    let name = foodList[i].foodname
-    let score = foodList[i].healthiness_score;
-    let ingredients = foodList[i].ingredients;
+    let name = foodinfo.foodname
+    let score = foodinfo.healthiness_score;
+    let ingredients = foodinfo.ingredients;
     const html = `
     <h2>${name}</h2>
     <div>Healthiness Score: ${score}/5</div>
     <div>Ingredients: ${ingredients}</div>    
 `;
-    node.innerHTML = html;
-
-    food.appendChild(node)
-//determine which food has the highest healthiness score and add the comment below that food. 
-    if(score>4 && score>foodList[i+1].healthiness_score){
-        const div = document.createElement("div");
-        div.classList.add("comment");
-        const divhtml = `This looks healthy and yummy. Let's eat ${name} today!`;
-        div.innerHTML = divhtml;
-        node.appendChild(div)
+//display a line of comment below that food. 
+    if(name === goodFood.foodname){
+  node.innerHTML = html + `<div class="comment">This looks healthy and yummy. Let's eat ${name} today!</div>`;
+    }else{
+        node.innerHTML = html
     }
-
-}
-
-
-
-
-
+    food.appendChild(node)
+})
