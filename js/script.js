e409f61f11a2c8a18f767be68f249d5dc89a8de3
sunnyyuanz <you@example.com>
2022-06-02 01:37:50 -0400

    added addListItem and eventlistener to IIFE

--- js/script.js
+++ js/script.js
@@ -19,43 +19,78 @@ let foodRepository = (function () {
         return foodList;
     }
 
+    function addListItem(food) {
+        const ulContainer = document.querySelector(".food ul");
+        const node = document.createElement("li");
+        let name = food.foodname;
+        let score = food.healthiness_score;
+        let ingredients = food.ingredients;
+        const button = document.createElement('button');
+        button.innerText = name;
+        button.classList.add("foodbutton");
+        const foodDetail = document.createElement('div');
+        foodDetail.innerHTML =`
+        <div>Healthiness Score: ${score}/5</div>
+        <div>Ingredients: ${ingredients}</div>
+        `;
+        foodDetail.classList.add("clicked");
+        node.appendChild(button);
+        node.appendChild(foodDetail);
+        ulContainer.appendChild(node);
+        button.addEventListener("click", function (){
+            showDetails(food)
+        })
+    }
+
+    function showDetails(food){
+        console.log(food)
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     }
 })()
 
 //Starting pull data from array to display in Html
-const food = document.querySelector(".food ul");
-let goodFood = { healthiness_score: 4 }
+// let goodFood = { healthiness_score: 4 }
 
 //getAll the data from food list
 foodRepository.getAll().forEach(foodinfo => {
-    buildHtml(foodinfo)
+    foodRepository.addListItem(foodinfo)
 })
+
+// function Checkanswersopen() {
+//     for (let i = 0; i < answers.length; i++) {
+//         questionBtns[i].classList.remove("open")
+//     }
+// }
 
 
 //one function should only do one job so I created a seperate function just to display content on html.
-function buildHtml(option) {
-    const node = document.createElement("li");
-    let name = option.foodname
-    let score = option.healthiness_score;
-    let ingredients = option.ingredients;
-    const html = `
-    <h2>${name}</h2>
-    <div>Healthiness Score: ${score}/5</div>
-    <div>Ingredients: ${ingredients}</div>    
-`;
-//determine which food has the best healthiness score
-    if (score > goodFood.healthiness_score) {
-        goodFood = option
-    }
-//display a line of comment below that food.
-    if (name === goodFood.foodname) {
-        node.innerHTML = html + `<div class="comment">This looks healthy and yummy. Let's eat ${name} today!</div>`;
-    } else {
-        node.innerHTML = html;
-    }
-    food.appendChild(node)
-}
+// function buildHtml(option) {
+//     const node = document.createElement("li");
+//     let name = option.foodname
+    
+//     const button = document.createElement('button');
+//     button.innerText = name;
+//     button.classList.add("foodbutton");
+//     node.appendChild(button);
+    // const html = `
+    // <div>Healthiness Score: ${score}/5</div>
+    // <div>Ingredients: ${ingredients}</div>
+    // `;
+//     //determine which food has the best healthiness score
+//     // if (score > goodFood.healthiness_score) {
+//     //     goodFood = option
+//     // }
+//     //display a line of comment below that food.
+//     // if (name === goodFood.foodname) {
+//     //     node.innerHTML = html + `<div class="comment">This looks healthy and yummy. Let's eat ${name} today!</div>`;
+//     // } else {
+//     //     node.innerHTML = html;
+//     // }
+//     food.appendChild(node)
+// }
 
